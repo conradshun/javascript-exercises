bc1dcefadef5aaeeb7e24c8b228fa846977c8c83
conrad <conradmativo25@gmail.com>
2026-08-10 20:00:22 +0800

    moved the both cases of fizz and buzz up

--- javascript.js
+++ javascript.js
@@ -88,13 +88,13 @@ console.log(lastLetter("javascript"));
 let answer = parseInt(prompt("Please enter a number you would like to FizzBuzz to: "))
 
 for (let i = 1; i <= answer; i++){
-    if(i % 3 == 0){
+    if(i % 3 == 0 && i % 5 == 0){
       console.log("Fizz");
     }
-    else if (i % 5 == 0){
+    else if (i % 3 == 0){
       console.log("Buzz");
     }
-    else if (i % 3 == 0 && i % 5 == 0){
+    else if (i % 5 == 0){
       console.log("FizzBuzz")
     }
     else{
