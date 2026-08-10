76b23acf1d472a245022f4b4456f63c58e8ab656
conrad <conradmativo25@gmail.com>
2026-08-10 19:58:38 +0800

    solved fizzbuzz problem

--- javascript.js
+++ javascript.js
@@ -88,5 +88,16 @@ console.log(lastLetter("javascript"));
 let answer = parseInt(prompt("Please enter a number you would like to FizzBuzz to: "))
 
 for (let i = 1; i <= answer; i++){
-  console.log(i);
+    if(i % 3 == 0){
+      console.log("Fizz");
+    }
+    else if (i % 5 == 0){
+      console.log("Buzz");
+    }
+    else if (i % 3 == 0 && i % 5 == 0){
+      console.log("FizzBuzz")
+    }
+    else{
+      console.log(i);
+    }
 }
