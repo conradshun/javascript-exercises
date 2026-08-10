bea8f1664fc17dd0a4e305285f71b8714a03dcdb
conrad <conradmativo25@gmail.com>
2026-08-10 11:27:43 +0800

    did 5 exercises involving functions

--- javascript.js
+++ javascript.js
@@ -43,3 +43,44 @@ const buttons = document.querySelectorAll("button");
 for (const button of buttons) {
   button.addEventListener("click", createParagraph);
 }
+
+const string = "  the revolution will not be televised"
+console.log(string);
+
+let name = "john";
+const greeting = `Hello ${name}`;
+console.log(greeting);
+
+const one = `hello, `;
+const two = `how are you`;
+const joined = `${one}${two}`;
+
+console.log(joined);
+
+function add7(number){
+
+  return number + 7;
+}
+
+console.log(add7(10));
+
+function multiply(number1, number2){
+
+  return number1*number2;
+
+}
+
+console.log(multiply(3,10));
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+console.log(capitalize("abcd"));
+
+function lastLetter(str){
+
+return str.at(-1);
+}
+
+console.log(lastLetter("javascript"));
